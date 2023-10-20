e5a32299fc3f10f72170076dcf777b37aefc063c
DaJaffaMan <jack.h.jefferies@gmail.com>
2023-10-20 12:14:51 +0100

    style(contact): adjust margin for contact icons and increase size of contact icons feat(contact): increase size of contact modal and contact icons The margin for the contact icons has been adjusted to improve spacing. The size of the contact modal has been increased to provide a better user experience. Additionally, the size of the contact icons has been increased to make them more visually prominent.

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -14,7 +14,7 @@ interface ContactModalProps {
 
 const ContactModal: React.FC<ContactModalProps> = ({ show, onHide }) => {
   return (
-    <Modal show={show} onHide={onHide} size="sm" centered>
+    <Modal show={show} onHide={onHide} size="lg" centered className="">
       <Modal.Header closeButton>
         <Modal.Title>Contact Me</Modal.Title>
       </Modal.Header>
@@ -22,21 +22,21 @@ const ContactModal: React.FC<ContactModalProps> = ({ show, onHide }) => {
         <div className="contact-icons">
           <a href="https://www.linkedin.com/in/jack-jefferies" target="_blank" rel="noopener noreferrer">
             <div className="contact-icon">
-              <FontAwesomeIcon icon={faLinkedin} size="2x" className="rounded-icon" />
+              <FontAwesomeIcon icon={faLinkedin} size="3x" className="rounded-icon" />
               <p className="contact-text">LinkedIn</p>
             </div>
           </a>
 
           <a href="https://github.com/Dajaffaman" target="_blank" rel="noopener noreferrer">
             <div className="contact-icon">
-              <FontAwesomeIcon icon={faGithub} size="2x" className="rounded-icon" />
+              <FontAwesomeIcon icon={faGithub} size="3x" className="rounded-icon" />
               <p className="contact-text">GitHub</p>
             </div>
           </a>
 
           <a href="mailto:jack@etherealtechnologies.co.uk">
             <div className="contact-icon">
-              <FontAwesomeIcon icon={faEnvelope} size="2x" className="rounded-icon" />
+              <FontAwesomeIcon icon={faEnvelope} size="3x" className="rounded-icon" />
               <p className="contact-text">Email</p>
             </div>
           </a>
